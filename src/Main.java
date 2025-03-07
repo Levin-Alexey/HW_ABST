2554d8bb11b6d164dd801399b6b51b6a830da7ef
Levin-Alexey <levin.penza@yandex.ru>
2025-03-07 08:47:12 +0300

    added manual testing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import logger.Logger;
+import logger.SmartLogger;
 
 public class Main {
     public static void main(String[] args) {
-
+        Logger logger = new SmartLogger();
+        logger.log("System started");
+        logger.log("System error");
+        logger.log("System stopped");
     }
 }
